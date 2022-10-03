997111288b74f27f15df6c66a6ea834df07dd51a
Gonzalo <gonzaloburgosdelahera@|gmail.com>
2022-10-03 23:13:23 +0200

    Use revision and CFM in encrypt/decrypt

--- src/lib.rs
+++ src/lib.rs
@@ -36,26 +36,16 @@ macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-struct Config {
-    use_aes: bool
-}
-
 const PADDING: [u8; 32] = [0x28, 0xBF, 0x4E, 0x5E, 0x4E, 0x75, 0x8A, 0x41, 0x64, 0x00, 0x4E, 0x56, 0xFF, 0xFA, 0x01, 0x08,
                            0x2E, 0x2E, 0x00, 0xB6, 0xD0, 0x68, 0x3E, 0x80, 0x2F, 0x0C, 0xA9, 0xFE, 0x64, 0x53, 0x69, 0x7A];
-static CFG: Mutex<Config> = Mutex::new(Config { use_aes: false });
 
 type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
 type Aes128CbcEnc = cbc::Encryptor<aes::Aes128>;
 
-#[wasm_bindgen]
-pub fn config(use_aes: bool) {
-    CFG.lock().unwrap().use_aes = use_aes;
-}
-
 // https://opensource.adobe.com/dc-acrobat-sdk-docs/standards/pdfstandards/pdf/PDF32000_2008.pdf
 
 #[wasm_bindgen]
-pub fn encrypt(obj_num: i32, gen_num: i32, key: Vec<u8>, stream: Vec<u8>) -> Vec<u8> {
+pub fn encrypt(obj_num: i32, gen_num: i32, key: Vec<u8>, stream: Vec<u8>, rev: i32, cfm: &str) -> Vec<u8> {
     let obj_num = &obj_num.to_le_bytes()[0..3];
     let gen_num = &gen_num.to_le_bytes()[0..2];
 
@@ -63,56 +53,22 @@ pub fn encrypt(obj_num: i32, gen_num: i32, key: Vec<u8>, stream: Vec<u8>) -> Vec
     new_key.append(&mut obj_num.to_vec());
     new_key.append(&mut gen_num.to_vec());
 
-    let mut data = stream;
+    let data = stream;
 
-    if CFG.lock().unwrap().use_aes {
-        // AES (Testing needed)
-        console_log!("Using AES");
-
-        new_key.append(&mut vec![0x73, 0x41, 0x6C, 0x54]);
-
-        let hash = md5::compute(new_key);
-
-        let key = hash.0;
-
-        let mut iv = [0x00u8; 16];
-        for i in &mut iv {
-            *i = rand::random::<u8>();
-        }
-        let mut iv_vec = iv.to_vec();
-
-        let data_len = data.len();
-
-        let div = (data_len + 16) / 16;
-        let mut data_extended = vec![0x00; div * 16];
-        for (pos, element) in data.iter().enumerate() {
-            data_extended[pos] = *element;
-        }
-
-        let mut pt = Aes128CbcEnc::new(&key.into(), &iv.into())
-            .encrypt_padded_mut::<Pkcs7>(&mut data_extended, data_len)
-            .unwrap_or_default()
-            .to_vec();
-
-        iv_vec.append(&mut pt);
-
-        iv_vec
-
+    if rev < 4 {
+        use_rc4(data, new_key)
     } else {
-        // RC4
-        console_log!("Using RC4");
-
-        let hash = md5::compute(new_key);
-
-        let mut rc4 = Rc4::<U16>::new(hash.as_slice().into());
-        rc4.apply_keystream(&mut data);
-
-        data
+        match cfm {
+            "None" => data,
+            "V2" => use_rc4(data, new_key),
+            "AESV2" => use_aes_encrypt(data, new_key),
+            _ => panic!("Wrong CFM")
+        }
     }
 }
 
 #[wasm_bindgen]
-pub fn decrypt(obj_num: i32, gen_num: i32, key: Vec<u8>, stream: Vec<u8>) -> Vec<u8> {
+pub fn decrypt(obj_num: i32, gen_num: i32, key: Vec<u8>, stream: Vec<u8>, rev: i32, cfm: &str) -> Vec<u8> {
     let obj_num = &obj_num.to_le_bytes()[0..3];
     let gen_num = &gen_num.to_le_bytes()[0..2];
 
@@ -120,40 +76,83 @@ pub fn decrypt(obj_num: i32, gen_num: i32, key: Vec<u8>, stream: Vec<u8>) -> Vec
     new_key.append(&mut obj_num.to_vec());
     new_key.append(&mut gen_num.to_vec());
 
-    let mut data = stream;
+    let data = stream;
 
-    if CFG.lock().unwrap().use_aes {
-        // AES (Testing needed)
-        console_log!("Using AES");
-
-        new_key.append(&mut vec![0x73, 0x41, 0x6C, 0x54]);
-
-        let hash = md5::compute(new_key);
-
-        let key = hash.0;
-
-        let mut iv = [0x00u8; 16];
-        for (elem, i) in data.iter().zip(0..16) {
-            iv[i] = *elem;
-        }
-
-        let pt = Aes128CbcDec::new(&key.into(), &iv.into())
-            .decrypt_padded_mut::<Pkcs7>(&mut data[16..])
-            .unwrap();
-
-        pt.to_vec()
-
+    if rev < 4 {
+        use_rc4(data, new_key)
     } else {
-        // RC4
-        console_log!("Using RC4");
-
-        let hash = md5::compute(new_key);
-
-        let mut rc4 = Rc4::<U16>::new(hash.as_slice().into());
-        rc4.apply_keystream(&mut data);
-
-        data
+        match cfm {
+            "None" => data,
+            "V2" => use_rc4(data, new_key),
+            "AESV2" => use_aes_encrypt(data, new_key),
+            _ => panic!("Wrong CFM")
+        }
     }
+}
+
+fn use_rc4(mut data: Vec<u8>, key: Vec<u8>) -> Vec<u8> {
+    console_log!("Using RC4");
+
+    let hash = md5::compute(key);
+
+    let mut rc4 = Rc4::<U16>::new(hash.as_slice().into());
+    rc4.apply_keystream(&mut data);
+
+    data
+}
+
+fn use_aes_encrypt(data: Vec<u8>, mut key: Vec<u8>) -> Vec<u8> {
+    console_log!("Using AES");
+
+    key.append(&mut vec![0x73, 0x41, 0x6C, 0x54]);
+
+    let hash = md5::compute(key);
+
+    let key = hash.0;
+
+    let mut iv = [0x00u8; 16];
+    for i in &mut iv {
+        *i = rand::random::<u8>();
+    }
+    let mut iv_vec = iv.to_vec();
+
+    let data_len = data.len();
+
+    let div = (data_len + 16) / 16;
+    let mut data_extended = vec![0x00; div * 16];
+    for (pos, element) in data.iter().enumerate() {
+        data_extended[pos] = *element;
+    }
+
+    let mut pt = Aes128CbcEnc::new(&key.into(), &iv.into())
+        .encrypt_padded_mut::<Pkcs7>(&mut data_extended, data_len)
+        .unwrap_or_default()
+        .to_vec();
+
+    iv_vec.append(&mut pt);
+
+    iv_vec
+}
+
+fn use_aes_decrypt(mut data: Vec<u8>, mut key: Vec<u8>) -> Vec<u8> {
+    console_log!("Using AES");
+
+    key.append(&mut vec![0x73, 0x41, 0x6C, 0x54]);
+
+    let hash = md5::compute(key);
+
+    let key = hash.0;
+
+    let mut iv = [0x00u8; 16];
+    for (elem, i) in data.iter().zip(0..16) {
+        iv[i] = *elem;
+    }
+
+    let pt = Aes128CbcDec::new(&key.into(), &iv.into())
+        .decrypt_padded_mut::<Pkcs7>(&mut data[16..])
+        .unwrap();
+
+    pt.to_vec()
 }
 
 #[wasm_bindgen]
